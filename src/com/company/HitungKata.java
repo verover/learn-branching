7de00655f555f3bff74df28be164757c63b934d7
RyanJuan <juanprojectt@gmail.com>
2021-12-04 22:19:34 +0700

    add code in HitungJava

--- src/com/company/HitungKata.java
+++ src/com/company/HitungKata.java
@@ -1,4 +1,18 @@
 package com.company;
 
 public class HitungKata {
+    static int hitungKata(String kata){
+        int jml=0;
+        char jumlah[] = new char[kata.length()];
+        for(int i = 0; i < kata.length(); i++){
+            jumlah[i] = kata.charAt(i);
+            if(((i > 0) && (jumlah[i] !=' ') && (jumlah[i - 1] == ' ') || ((jumlah[0] != ' ') && (i == 0))))
+                jml++;
+        }
+        return jml;
+    }
+    public static void main(String[] args) {
+        String string = "satu dua tiga";
+        System.out.println(hitungKata(string));
+    }
 }
