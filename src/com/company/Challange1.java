56d103f87c764cb89baa6a673f029209f552eebf
--replace-all <azzahradindas@gmail.com>
2021-12-04 20:39:15 +0700

    add random methods

--- src/com/company/Challange1.java
+++ src/com/company/Challange1.java
@@ -3,4 +3,28 @@ package com.company;
 public class Challange1 {
     int score;
     String name;
+
+    public Challange1() {
+        super();
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
