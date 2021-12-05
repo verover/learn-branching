c73040443538729492ba39dbc2ba1bdef786ea2c
mhmdiqbalakbari <mhmdiqbalakbari@gmail.com>
2021-12-05 12:12:22 +0700

    [FIX] menu menampilkan welcomegi

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,13 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Challange1 ch1 = new Challange1();
-        ch1.name = "gaaaaa";
-        System.out.println("ASKJFKJFALKDASLJFKL");
+        //Challange1 ch1 = new Challange1();
+        //ch1.name = "gaaaaa";
+        //System.out.println("ASKJFKJFALKDASLJFKL");
+        Menu daftarMenu = new Menu();
+        daftarMenu.welcome();
     }
 }
--- src/com/company/Menu.java
+++ src/com/company/Menu.java
@@ -1,9 +1,9 @@
 package com.company;
 
-public enum Menu {
-    CHALLANGE1, CHALLANGE2, CHALLANGE3, EXIT, OPEN;
+public class Menu {
+    //CHALLANGE1, CHALLANGE2, CHALLANGE3, EXIT, OPEN;
 
-    Menu() {
+    public void welcome() {
         System.out.println("WELCOME");
     }
     public void Test(){
