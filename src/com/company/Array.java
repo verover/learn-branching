a3b33c1947bd9a4817bdf0bbd11e9121d17e84f4
Brian Bevinius E <ananda8811@gmail.com>
2021-12-05 12:49:37 +0700

    challange 2

--- src/com/company/Array.java
+++ src/com/company/Array.java
@@ -6,33 +6,33 @@ import java.util.Scanner;
 public class Array {
     public static void main(String[] args) {
         //int[] array = new int[10];
+        pasanganAngka();
+        }
+        // System.out.println(sum);
+
+    public static void pasanganAngka() {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         //int [] array = {-5, 5, 0, 7, -3, 2};
-        int [] arraysatu = {-5, 10, 0, 7, -3, 2};
-        int [] [] array2d = { {1,2} , {3,6} };
+        int[] arraysatu = {-5, 5, 0, 7, -3, 2};
+        int[][] array2d = {{1, 2}, {3, 6}};
         //int [] array = {2 , 0 , 3};
-
+        int [][] arraytampung;
        /* int sum = 0;
         for (int i = 0; i < array.length; i++) {
             sum = sum +array[i];
         }*/
-       // System.out.println(Arrays.deepToString(array2d));
+        // System.out.println(Arrays.deepToString(array2d));
 
         for (int i = 1; i < arraysatu.length; i++) {
             for (int j = 0; j < arraysatu.length; j++) {
-                if (arraysatu[j]+arraysatu[i] == n) {
-                    if(arraysatu[j] != arraysatu[i]) {
+                if (arraysatu[j] + arraysatu[i] == n) {
+                    if (arraysatu[j] != arraysatu[i]) {
                         System.out.println("[" + arraysatu[i] + "],[" + arraysatu[j] + "]");
                     }
-                }
-                else {
-
                 }
             }
 
         }
-        // System.out.println(sum);
     }
-
 }
